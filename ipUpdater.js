c1cb76a68e633d5782f5af3cdb4934968dcd3d7e
ludele <Luddesnek@proton.me>
2023-12-19 11:13:23 +0100

    Made algorithm to send local ip-adress

---
+++ ipUpdater.js
@@ -0,0 +1,53 @@
+import { MongoClient } from "mongodb";
+import os from "os";
+
+async function getLocalIpAddress() {
+  const ifaces = os.networkInterfaces();
+  let localIpAddress = "";
+
+  Object.keys(ifaces).forEach((ifname) => {
+    ifaces[ifname].forEach((iface) => {
+      if (iface.family === "IPv4" && !iface.internal) {
+        localIpAddress = iface.address;
+      }
+    });
+  });
+
+  return localIpAddress;
+}
+
+async function updatePiIpInDatabase(url, dbName, piIp) {
+  try {
+    const client = await MongoClient.connect(url, { useUnifiedTopology: true });
+    const db = client.db(dbName);
+
+    await db.collection("ip").updateOne(
+      {},
+      { $set: { "pi-ip": piIp } }
+    );
+
+    console.log("Pi IP address updated in the database.");
+    await client.close();
+  } catch (error) {
+    console.error("Error updating Pi IP address in the database:", error);
+  }
+}
+
+async function updatePiIp(url, dbName) {
+  try {
+    const localIpAddress = await getLocalIpAddress();
+
+    await updatePiIpInDatabase(url, dbName, localIpAddress);
+  } catch (error) {
+    console.error("Error updating Pi IP:", error);
+  }
+}
+
+if (require.main === module) {
+  const url = "";
+  const dbName = "ip";
+
+  updatePiIp(url, dbName);
+}
+
+export default updatePiIp;
